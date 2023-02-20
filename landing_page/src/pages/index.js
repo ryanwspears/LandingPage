c1e1a6654f8d62ffa85a150cdd7ada796d4414b6
Ryan Spears <spearswesley5@gmail.com>
2023-02-20 00:08:48 -0600

    Update index.js

--- landing_page/src/pages/index.js
+++ landing_page/src/pages/index.js
@@ -4,7 +4,6 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import axios from 'axios'
 import { useState } from 'react'
-import pool from 'config/db'
 
 export default function Home() {
 
